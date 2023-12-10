4fb8122a56c3a78f0bd789bcf165f78b45e27604
Djuslun <djuslun@yandex.ru>
2023-12-10 10:58:06 +0300

    add user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,59 @@
+class Usercontroller {
+  async registration(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async block(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+}
+
+module.exports = new Usercontroller()
--- router/index.js
+++ router/index.js
@@ -1,13 +1,13 @@
 const Router = require('express')
-
+const usercontroller = require('../controllers/user-controller')
 const router = new Router()
 
-router.post('/registration')
-router.post('/login')
-router.post('/logout')
-router.patch('/block')
-router.delete('/delete')
-router.get('/refresh')
-router.get('/users')
+router.post('/registration', usercontroller.registration)
+router.post('/login', usercontroller.login)
+router.post('/logout', usercontroller.logout)
+router.patch('/block', usercontroller.block)
+router.delete('/delete', usercontroller.delete)
+router.get('/refresh', usercontroller.refresh)
+router.get('/users', usercontroller.getUsers)
 
-module.exports = { router }
+module.exports = router 
