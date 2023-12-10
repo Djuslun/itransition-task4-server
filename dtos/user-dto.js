265940876f3ac1d9aaa9d1ac677701e872454bb6
Djuslun <djuslun@yandex.ru>
2023-12-10 20:13:20 +0300

    fix user dto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -3,11 +3,13 @@ module.exports = class UserDto {
   id;
   isActive;
   last_login;
+  name;
 
   constructor(model) {
     this.email = model.email
     this.id = model._id
     this.isActive = model.isActive
     this.last_login = new Date()
+    this.name = model.name
   }
 }
